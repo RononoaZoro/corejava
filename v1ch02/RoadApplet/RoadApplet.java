e26e40d4be70be370b9361a4ad5dd154ae974a4a
luoxz <luoxz@akulaku.com>
2021-12-08 12:52:14 +0800

    加上目录

--- v1ch02/RoadApplet/RoadApplet.java
+++ v1ch02/RoadApplet/RoadApplet.java
@@ -2,20 +2,24 @@ import java.awt.*;
 import java.applet.*;
 import javax.swing.*;
 
-public class RoadApplet extends JApplet
-{  
-   private RoadComponent roadComponent;
-   private JSlider slowdown;
-   private JSlider arrival;
+//javac RoadApplet.java
+//打jar包的命令
+//jar cvfm RoadApplet.jar RoadApplet.mf *.dass
+//这是 JDK 自带的一个工具， 可以用来快速测试 applet
+//appletviewer RoadApplet.htnil
 
-   public void init()
-   {
-      EventQueue.invokeLater(() ->
-         {
+public class RoadApplet extends JApplet {
+    private RoadComponent roadComponent;
+    private JSlider slowdown;
+    private JSlider arrival;
+
+    public void init() {
+        EventQueue.invokeLater(() ->
+        {
             roadComponent = new RoadComponent();
-            slowdown = new JSlider(0, 100, 10);    
+            slowdown = new JSlider(0, 100, 10);
             arrival = new JSlider(0, 100, 50);
-        
+
             JPanel p = new JPanel();
             p.setLayout(new GridLayout(1, 6));
             p.add(new JLabel("Slowdown"));
@@ -23,24 +27,25 @@ public class RoadApplet extends JApplet
             p.add(new JLabel(""));
             p.add(new JLabel("Arrival"));
             p.add(arrival);
-            p.add(new JLabel("")); 
+            p.add(new JLabel(""));
             setLayout(new BorderLayout());
             add(p, BorderLayout.NORTH);
             add(roadComponent, BorderLayout.CENTER);
-         });
-   }
-   
-   public void start()
-   {
-      new Thread(() -> 
-         {
-            for (;;) 
-            {
-               roadComponent.update(
-                  0.01 * slowdown.getValue(),
-                  0.01 * arrival.getValue());
-               try { Thread.sleep(50); } catch(InterruptedException e) {}
+        });
+    }
+
+    public void start() {
+        new Thread(() ->
+        {
+            for (; ; ) {
+                roadComponent.update(
+                        0.01 * slowdown.getValue(),
+                        0.01 * arrival.getValue());
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                }
             }
-         }).start();
-   }
+        }).start();
+    }
 }
